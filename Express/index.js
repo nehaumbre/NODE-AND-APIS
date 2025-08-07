1940cab9c20dbf62ea64c7690774ee04f92a6bd9
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-08-07 19:13:54 +0530

    express commit into repo
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- Express/index.js
+++ Express/index.js
@@ -7,4 +7,7 @@ app.get("/", (req, res) => res.send("This is Home page"));
 app.get("/about", (req, res) => res.send("This is About Page"));
 
 app.get("/contact", (req, res) => res.send("This is Contact Page"));
+
+
+app.get("/blog", (req, res) => res.send("This is Blog Page"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
